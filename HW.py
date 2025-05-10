8aa2ec2325ae520ba5628b1cbd095b8092f14b83
mohdahmad2013 <ziena.saif@gmail.com>
2025-05-10 03:02:46 -0700

    Add files via upload

---
+++ HW.py
@@ -0,0 +1,9 @@
+import array as ar
+na={1,3,4,6,7}
+na2={1,2,3,7,9}
+r1=na.symmetric_difference(na2)
+print("Symetric difference is ",r1)
+c1={"Red","Green"}
+c2={"Green","Pink"}
+r2=c1.symmetric_difference(c2)
+print("Symetric difference is ",r2)
